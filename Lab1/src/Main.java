f0f22f7fc9575487d7e077fbc01f6053db211b8e
Maxim Elushev <maxim.elushev@yandex.ru>
2023-02-11 14:52:27 +0300

    Приведение работы к единому стилю

--- Lab1/src/Main.java
+++ Lab1/src/Main.java
@@ -1,16 +1,17 @@
 public class Main {
-    public static void main(String[] args) {
-        int evenSum = 0;
-        int oddSum = 0;
-        for (int i = 0; i < args.length; i++) {
-            int number = Integer.parseInt(args[i]);
-            if (number % 2 == 0) {
-                evenSum += number;
-            } else {
-                oddSum += number;
-            }
-        }
-        System.out.println("Сумма чётных чисел в последовательности: " + evenSum);
-        System.out.println("Сумма нечётных чисел в последовательности: " + oddSum);
+
+  public static void main(String[] args) {
+    int evenSum = 0;
+    int oddSum = 0;
+    for (int i = 0; i < args.length; i++) {
+      int number = Integer.parseInt(args[i]);
+      if (number % 2 == 0) {
+        evenSum += number;
+      } else {
+        oddSum += number;
+      }
     }
+    System.out.println("Сумма чётных чисел в последовательности: " + evenSum);
+    System.out.println("Сумма нечётных чисел в последовательности: " + oddSum);
+  }
 }
