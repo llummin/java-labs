b5aa3d5b9f1bc8a9056d0dd07c6e52add4b40c4c
Maxim Elushev <maxim.elushev@yandex.ru>
2023-02-26 20:29:35 +0300

    Удалил ненужные файлы

--- Lab2/src/SecondLab/InvalidNumberException.java
+++
@@ -1,9 +0,0 @@
-package SecondLab;
-
-// Исключение при валидации чисел
-public class InvalidNumberException extends Exception implements MyConstants {
-
-  public String toString() {
-    return "ОШИБКА: В последовательности есть число, которое меньше некоторого числа " + MIN_VALUE;
-  }
-}
