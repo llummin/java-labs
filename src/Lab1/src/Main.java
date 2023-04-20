7a13085a4bb4ab62eca82a9cb4ecfd3b11e55ba2
Maxim Elushev <maxim.elushev@yandex.ru>
2023-04-20 11:05:39 +0300

    code refactoring

--- src/Lab1/src/Main.java
+++ src/Lab1/src/Main.java
@@ -1,20 +1,23 @@
+import static java.lang.System.*;
+
 public class Main {
 
   public static void main(String[] args) {
     // Переменные, в которых будут храниться суммы чётных и нечётных чисел
     int evenSum = 0;
     int oddSum = 0;
+
     // Проходим по всем элементам последовательности
-    for (int i = 0; i < args.length; i++) {
+    for (String arg : args) {
       // Преобразуем текущий элемент последовательности в число
-      int number = Integer.parseInt(args[i]);
+      int number = Integer.parseInt(arg);
       if (number % 2 == 0) {
         evenSum += number;
       } else {
         oddSum += number;
       }
     }
-    System.out.println("Сумма чётных чисел в последовательности: " + evenSum);
-    System.out.println("Сумма нечётных чисел в последовательности: " + oddSum);
+    out.println("Сумма чётных чисел в последовательности: " + evenSum);
+    out.println("Сумма нечётных чисел в последовательности: " + oddSum);
   }
 }
