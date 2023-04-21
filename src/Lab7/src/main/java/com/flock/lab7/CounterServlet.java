575f89f1cefa2b4ff5fc887559061ce55256cfd9
Maxim Elushev <maxim.elushev@yandex.ru>
2023-04-21 03:17:19 +0300

    code refactoring

--- src/Lab7/src/main/java/com/flock/lab7/CounterServlet.java
+++ src/Lab7/src/main/java/com/flock/lab7/CounterServlet.java
@@ -11,6 +11,9 @@ public class CounterServlet extends HttpServlet {
   private int counter = 0;
   private int fontSize = 22;
 
+  private static final String TABLE_ROW_START = "<tr><td>";
+  private static final String TABLE_ROW_END = "</td></tr>";
+
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setCharacterEncoding("UTF-8");
@@ -37,21 +40,22 @@ public class CounterServlet extends HttpServlet {
     // Уменьшение размера шрифта на 2 пикселя
     fontSize = Math.max(fontSize - 2, minFontSize);
 
-    out.println("<style>body { font-size: 20px; } table { font-size: " + fontSize + "px; }</style>");
+    out.println(
+        "<style>body { font-size: 22px; } table { font-size: " + fontSize + "px; }</style>");
     out.println("<html><body>");
 
     // Вывод информации о студенте
     String fullName = "Елушев Максим Андреевич";
-    out.println("<p>Студент: " + fullName + "</p>");
+    out.println(TABLE_ROW_START + "Студент: " + fullName + TABLE_ROW_END);
     int groupNumber = 4317;
-    out.println("<p>Номер группы: " + groupNumber + "</p>");
+    out.println(TABLE_ROW_START + "Номер группы: " + groupNumber + TABLE_ROW_END);
 
     // Вывод счетчика и размера шрифта (начало таблицы)
     out.println("<style>table, td { border: none; }</style>");
     out.println("<table><tbody>");
-    out.println("<tr><td>" + "Счетчик: " + counter + "</td></tr>");
-    out.println("<tr><td>" + "Размер шрифта: " + fontSize + "</td></tr>");
-    out.println("<tr><td>&nbsp;</td></tr>"); // пустая строка
+    out.println(TABLE_ROW_START + "Счетчик: " + counter + TABLE_ROW_END);
+    out.println(TABLE_ROW_START + "Размер шрифта: " + fontSize + TABLE_ROW_END);
+    out.println(TABLE_ROW_START + "&nbsp;" + TABLE_ROW_END); // пустая строка
 
     // Основная функция
     int[] sequence = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
@@ -64,14 +68,15 @@ public class CounterServlet extends HttpServlet {
         oddSum += number;
       }
     }
-    out.println("<tr><td>" + "Результат:" + "</td></tr>");
-    out.println("<tr><td>" + "Сумма чётных чисел в последовательности: " + evenSum + "</td></tr>");
-    out.println("<tr><td>" + "Сумма нечётных чисел в последовательности: " + oddSum + "</td></tr>");
+
+    out.println(TABLE_ROW_START + "Результат:" + TABLE_ROW_END);
+    out.println(TABLE_ROW_START + "Сумма чётных чисел: " + evenSum + TABLE_ROW_END);
+    out.println(TABLE_ROW_START + "Сумма нечётных чисел: " + oddSum + TABLE_ROW_END);
 
     // Проверяем, можно ли еще уменьшить размер шрифта
     if (fontSize <= minFontSize) {
-      out.println("<tr><td>&nbsp;</td></tr>");
-      out.println("<tr><td>" + "Не в таблице!" + "</td></tr>");
+      out.println(TABLE_ROW_START + "&nbsp;" + TABLE_ROW_END);
+      out.println(TABLE_ROW_START + "Не в таблице!" + TABLE_ROW_END);
     }
 
     out.println("</table>");
